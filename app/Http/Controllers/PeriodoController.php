a3f2e0e41d50d7a35f876a5829acd900cbd877af
thegreatyamori <jersonmorocho16@gmail.com>
2019-09-24 21:43:34 -0500

    Crear periodos

--- app/Http/Controllers/PeriodoController.php
+++ app/Http/Controllers/PeriodoController.php
@@ -17,8 +17,28 @@ class PeriodoController extends Controller {
 		return view('periodo');
 	}
 
-	public function FunctionName()
+	public function crear()
 	{
-		# code...
+		return view('periodo.create');
+	}
+	public function guardar(Request $request)
+	{
+		\SGlab\Periodo::create([
+			'PER_NOMBRE' => $request['per_nombre'], 
+			'PER_ESTADO' => $request['per_estado'],
+			'PER_HORAS_ATENCION' => $request['per_horas_atencion'],
+			'PER_FECHA_INICIO' => $request['per_fecha_inicio'],
+			'PER_FECHA_FIN' => $request['per_fecha_fin']
+		]);
+
+		return view('periodo.success', ['title' => 'Periodo registrado!', 'subtitle' => 'El registro del periodo acádemico se ha realizado con éxito.']);
+	}
+	public function actualizar()
+	{
+		return view('periodo.update');
+	}
+	public function eliminar()
+	{
+		return "aqui esta";
 	}
 }
---
+++ resources/views/periodo/update.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.principal')
+
+@section('periodo')
+
+<div class="container">
+    <h2>Actualizar Periodo</h2>
+    <form action="{{url('/periodo/guardar')}}" method="POST">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="form-group">
+            <label for="per_nombre">Nombre</label>
+            <input type="input" class="form-control" id="per_nombre" name="per_nombre" placeholder="Nombre del periodo">
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label for="per_estado">Estado</label>
+                    <select class="form-control" id="per_estado" name="per_estado">
+                    <option>0</option>
+                    <option>1</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="per_horas_atencion">Horas Atencion</label>
+                    <input type="number" class="form-control" id="per_horas_atencion" name="per_horas_atencion" min="1" max="15">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label for="per_fecha_inicio">Fecha Inicio</label>
+                    <input type="date" class="form-control" id="per_fecha_inicio" name="per_fecha_inicio">
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="per_fecha_fin">Fecha Fin</label>
+                    <input type="date" class="form-control" id="per_fecha_fin" name="per_fecha_fin">
+                </div>
+            </div>
+        </div>
+        <!-- Submit Button -->
+        <button type="submit" class="btn btn-primary mb-2">Crear</button>
+        <a href="/periodo" class="btn btn-danger mb-2">Cancelar</a>
+    </form>
+</div>
+
+@stop
