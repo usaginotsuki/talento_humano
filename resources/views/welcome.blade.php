2c5827b862e9935b927d3b362bd6e4a1d35534db
David <davidaec27@gmail.com>
2019-09-25 22:39:56 -0500

    Agregado el código de Andrea

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,14 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
-</html>
+@extends('template.main')
+@section('content')
+    
+    <div class="mx-auto my-5 content" style="position: absolute; left: 50px"> 
+        <img src="imagenes/principal/icono_guia.png" >
+    </div>
+<div class="mx-auto my-5 content" style=" position: absolute; "> 
+        
+    </div>
+    <div class="mx-auto my-5 content" style=" text-align: center; width: 700px; height: 700px;"> 
+        <img src="imagenes/principal/espe.png" class="d-block w-100" alt="Responsive image">
+    </div>
+   
+  @endsection
