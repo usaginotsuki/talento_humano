25cdead3abb7c678bb034fcd82764f70528cdecf
lorena <mishu_03@hotmail.es>
2019-10-02 09:14:36 -0500

    creacion de archivos para los modulos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,3 +28,17 @@ Route::post('carrera/store', 'CarreraController@store');
 Route::get('carrera/{id}/edit', 'CarreraController@edit');
 Route::post('carrera/update', 'CarreraController@update');
 Route::post('carrera/{id}/destroy', 'CarreraController@destroy');
+
+Route::get('campus', 'CampusController@index');
+Route::get('campus/create', 'CampusController@create');
+Route::post('campus/store', 'CampusController@store');
+Route::get('campus/{id}/edit', 'CampusController@edit');
+Route::post('campus/update', 'CampusController@update');
+Route::post('campus/{id}/destroy', 'CampusController@destroy');
+
+Route::get('carrera', 'CarreraController@index');
+Route::get('carrera/create', 'CarreraController@create');
+Route::post('carrera/store', 'CarreraController@store');
+Route::get('carrera/{id}/edit', 'CarreraController@edit');
+Route::post('carrera/update', 'CarreraController@update');
+Route::post('carrera/{id}/destroy', 'CarreraController@destroy');
