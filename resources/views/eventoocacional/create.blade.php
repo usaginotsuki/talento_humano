c954b4e1274131d4ae0c597aa35146de011cfe1c
Jean Pierre <jppineda1@espe.edu.ec>
2019-11-12 21:50:47 -0500

    ReporteEventoOcasional

--- resources/views/eventoocacional/create.blade.php
+++ resources/views/eventoocacional/create.blade.php
@@ -4,10 +4,10 @@
 
 <div class="container-fluid">
 <h2>Crear Hora Evento Ocasional</h2>
-    <form action="{{url('/eventoocacional/store')}}" method="POST">
+    <form action="{{url('/ocasionales/store')}}" method="POST">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="col">
-            <div class="row">
+        <div class="row">
+            <div class="col">
                 <div class="form-group">
                     <label for="LAB_CODIGO">Sala</label>
                     <select type="input" class="form-control" id="LAB_CODIGO" name="LAB_CODIGO" placeholder="Laboratorio"  required>
@@ -17,7 +17,8 @@
                         @endforeach
                     </select> 
                 </div>
-                <div class="row">
+            </div>
+            <div class="col">
                 <div class="form-group">
                     <label for="MAT_CODIGO">Materia</label>
                     <select type="input" class="form-control" id="MAT_CODIGO" name="MAT_CODIGO" placeholder="Materia"  required>
@@ -29,9 +30,9 @@
             </div>
         </div>
         
-        <div class="col">
+        <div class="row">
             
-            <div class="row">
+            <div class="col">
                 <div class="form-group">
                     <label for="DOC_CODIGO">Docente</label>
                     <select type="input" class="form-control" id="DOC_CODIGO" name="DOC_CODIGO" placeholder="Docente"  required>
@@ -42,33 +43,36 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="row">
-                    <div class="form-group">
-                        <label for="CON_DIA">Dia</label>
-                        <input type="date" class="form-control" id="CON_DIA" name="CON_DIA" required>
-                    </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="CON_DIA">Dia</label>
+                    <input type="date" class="form-control" id="CON_DIA" name="CON_DIA" required>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="row">
+        <div class="row">
+            <div class="col">
                 <div class="form-group">
                     <label for="CON_HORA_ENTRADA">Hora Entrada</label>
                     <input type="time" class="form-control" id="CON_HORA_ENTRADA" name="CON_HORA_ENTRADA" required>
                 </div>
+            </div>    
+            <div class="col">
                 <div class="form-group">
                     <label for="CON_HORA_SALIDA">Hora Salida</label>
                     <input type="time" class="form-control" id="CON_HORA_SALIDA" name="CON_HORA_SALIDA" required>
                 </div>
-            </div>
-            
+            </div>   
         </div>
-        <div class="col">
-            <div class="row">
+        <div class="row">
+            <div class="col">
                 <div class="form-group">
                     <label for="CON_NUMERO_HORAS">Numero Horas</label>
                     <input type="number" class="form-control" id="CON_NUMERO_HORAS" name="CON_NUMERO_HORAS" required>
                 </div>
+            </div>
+            <div class="col">
                 <div class="form-group">
                     <label for="CON_NOTA">Nota</label>
                     <input type="text" class="form-control" id="CON_NOTA" name="CON_NOTA">
--- resources/views/reportes/eventos.blade.php
+++ resources/views/reportes/eventos.blade.php
@@ -7,19 +7,28 @@
         <div class="col">
             <form action="{{url('/reporte/ocasionales')}}" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+   
                 <div class="card border-primary mb-3">
                     <div class="card-header text-primary">Consultar</div>
                     <div class="card-body text-primary">
-                    <select class="form-control" id="PER_CODIGO" name="PER_CODIGO"  >
-                    @foreach ($periodos as $per)
-                      @if($per->PER_CODIGO==$periodoActual)
-                        <option value="{{$per->PER_CODIGO}}" selected="{{$per->PER_CODIGO}}">{{$per->PER_NOMBRE}}</option>
-                      @else
-                      <option value="{{$per->PER_CODIGO}}"  >{{$per->PER_NOMBRE}}</option>
-                      @endif
-                    @endforeach
-                  </select> 
+                        <span style="font-weight: 300;">Periodo</span>
+                        <select required class="form-control" id="PER_CODIGO" name="PER_CODIGO"  >
+                            @foreach ($periodos as $per)
+                            @if($per->PER_CODIGO==$periodoActual)
+                                <option value="{{$per->PER_CODIGO}}" selected="{{$per->PER_CODIGO}}">{{$per->PER_NOMBRE}}</option>
+                            @else
+                                <option value="{{$per->PER_CODIGO}}"  >{{$per->PER_NOMBRE}}</option>
+                            @endif
+                            @endforeach
+                        </select> 
                        
+                    </div>
+                    <div class="card-body text-primary">
+                        <span style="font-weight: 300;">Fecha Inicial</span>
+                        <input required  type="date" class="form-control"   id="fechaInicial" name="fechaInicial" value="{{$fechaInicial}}" ></input>                    </div>
+                    <div class="card-body text-primary">
+                        <span style="font-weight: 300;">Fecha Final</span> 
+                        <input required type="date" class="form-control" id="fechaFinal" name="fechaFinal" value="{{$fechaFinal}}" ></input>
                     </div>
                     <div class="card-body text-primary">
                     <button type="submit" class="btn btn-primary"><span class="oi oi-magnifying-glass"></span> Consultar</button>
@@ -41,7 +50,11 @@
                     </form>
                     <br>    
                     @if (isset($data))
-                      <button onclick="exportEventoOcasional()" class="btn btn-info"><span class="oi oi-cloud-download"></span> Exportar a PDF</button>
+                        @if(count($data)>0)
+                             <a href="{{url('reporte/pdfevento/'.$periodoActual.'/'.$fechaInicial.'/'.$fechaFinal)}}" class="btn btn-info"><span class="oi oi-cloud-download"></span> Exportar a  PDF</a>
+                         @else
+                             <button disabled onclick="exportEventoOcasional()" class="btn btn-info"><span class="oi oi-cloud-download"></span> Exportar a PDF</button>
+                        @endif
                     @else
                        <button disabled onclick="exportEventoOcasional()" class="btn btn-info"><span class="oi oi-cloud-download"></span> Exportar a PDF</button>
                     @endif
@@ -68,14 +81,17 @@
             <span style="font-weight: 300;">EVENTO OCASIONAL</span>
         </span>
         <br>
-        <span class="h6" style="margin-right:150px;">PERIODO;
-        @if (isset($periodos[$periodoActual]))
-            <span style="font-weight: 300;">{{ $periodos[$periodoActual]->PER_NOMBRE }}</span>
+        <span class="h6" style="margin-right:150px;">PERIODO:{{$periodoActual}}
+        @if (count($data)>0)
+            <span style="font-weight: 300;">{{ $data[0]->PER_NOMBRE }}</span>
+        @else
+            <span style="font-weight: 300;">{{ $periodos[0]->PER_NOMBRE }}</span>
+
         @endif
-        </span>>
+        </span>
       
     </p>     
-    <table id="ListTable" class="table table-hover table-bordered results">
+    <table id="ListOC" class="table table-hover table-bordered results">
     <thead>
             <tr>
                 <th scope="row">ID</th>
@@ -90,25 +106,32 @@
             </tr>
         </thead>
         <tbody>
-        @if (isset($data))
-                @foreach ($data as $eve)
-                    
+            @if (count($data)>0))
+
+                    @foreach ($data as $eve)
+                        
+                        <tr>
+                            <td  scope="row">{{$eve-> CON_CODIGO}}</td>
+                            <td scope="row">{{$eve -> LAB_NOMBRE}}</td>
+                            <td scope="row">{{$eve -> MAT_NOMBRE}}</td>
+                            <td scope="row">{{$eve -> DOC_NOMBRE}}</td>
+                            <td scope="row">{{$eve -> CON_DIA }}</td>
+                            <td scope="row">{{$eve -> CON_HORA_ENTRADA}}</td>
+                            <td scope="row">{{$eve -> CON_HORA_SALIDA}}</td>
+                            <td scope="row">{{$eve -> CON_NUMERO_HORAS}}</td>
+                            <td scope="row">{{$eve-> CON_NOTA}}</td>
+                        </tr>
+                        
+                    @endforeach     
+            @else
+                <table align="center" >
                     <tr>
-                        <td scope="row">{{$eve-> CON_CODIGO}}</td>
-                        <td scope="row">{{$eve -> LAB_NOMBRE}}</td>
-                        <td scope="row">{{$eve -> MAT_NOMBRE}}</td>
-                        <td scope="row">{{$eve -> DOC_NOMBRE}}</td>
-                        <td scope="row">{{$eve -> CON_DIA }}</td>
-                        <td scope="row">{{$eve -> CON_HORA_ENTRADA}}</td>
-                        <td scope="row">{{$eve -> CON_HORA_SALIDA}}</td>
-                        <td scope="row">{{$eve -> CON_NUMERO_HORAS}}</td>
-                        <td scope="row">{{$eve-> CON_NOTA}}</td>
+                        <td scope="row"><h3>Valores no encontrados</h3></td>
                     </tr>
-                    
-                @endforeach     
-        @endif        
-        </tbody> 
-         </tbody>   
+                </table>
+            @endif        
+         
+    
         </tbody> 
 </table>
 
@@ -116,3 +139,4 @@
 </div>
 
 @endsection
+
