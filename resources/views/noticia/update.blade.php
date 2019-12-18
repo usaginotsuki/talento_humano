d11bb66948dfe7cdba0ede1a6c4106b2534f4ab6
Jean Pierre <jppineda1@espe.edu.ec>
2019-12-17 23:30:38 -0500

    CRUD NOTICIA
    TENER EN CUENTA AGREGAR ESTE COMANDO EN php.ini debido a que este modulo trabaja con imagenes
    extension=php_fileinfo.dll

--- resources/views/noticia/update.blade.php
+++ resources/views/noticia/update.blade.php
@@ -6,51 +6,44 @@
  -->
 @extends('app')
 @section('content')
-@include('shared.title', array('titulo' => 'Actualizar Objeto'))
+@include('shared.title', array('titulo' => 'Actualizar Noticia'))
     
 <div class="container-fluid">
-    <form action="{{url('/objeto/update')}}" method="POST" enctype="multipart/form-data">
+    <form action="{{url('/noticia/update')}}" method="POST" enctype="multipart/form-data">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="OBR_CODIGO" value="{{ $objeto->OBR_CODIGO }}">
+        <input type="hidden" name="NOT_CODIGO" value="{{ $noticia->NOT_CODIGO }}">
 
         <div class="container-fluid">
                 <p><h6>Los campos con <span style="color:#FF0000";>*</span> son obligatorios</h6></p>
-                <div class="row" >
+               
+                <div class="row">
                     <div class="col">
                         <div class="form-group">
-                            <label for="OBR_NOMBRE">Nombre<span style="color:#FF0000";>*</span></label>
-                            <input type="text" class="form-control" id="OBR_NOMBRE" name="OBR_NOMBRE" placeholder="Nombre del Objeto" value="{{ $objeto->OBR_NOMBRE }}" required>
+                            <label for="NOT_TITULO">Titulo<span style="color:#FF0000";>*</span></label>
+                            <input type="text" class="form-control" id="NOT_TITULO" name="NOT_TITULO" value="{{ $noticia->NOT_TITULO }}" placeholder="Titulo" required>
                         </div>
                     </div>
-                   
-            
-                 
+                    <div class="col">
+                        
+                        <div class="form-group">
+                            <label for="NOT_DESCRIPCION">Descripcion<span style="color:#FF0000";>*</span></label>
+                            <input type="text" class="form-control" id="NOT_DESCRIPCION" name="NOT_DESCRIPCION" value="{{ $noticia->NOT_DESCRIPCION }}"  required>
+                        </div>
+                    </div>
+                    
                 </div>
                 <div class="row">
                     <div class="col">
                         <div class="form-group">
-                            <label for="OBR_DESCRIPCION">Descripcion<span style="color:#FF0000";>*</span></label>
-                            <input type="text" class="form-control" id="OBR_DESCRIPCION" name="OBR_DESCRIPCION"  value="{{ $objeto->OBR_DESCRIPCION }}" required>
+                            <label for="NOT_FECHA_INICIO">Fecha Inicio<span style="color:#FF0000";>*</span></label>
+                            <input type="date" class="form-control" id="NOT_FECHA_INICIO" name="NOT_FECHA_INICIO"  value="{{ $noticia->NOT_FECHA_INICIO }}" required>
                         </div>
                     </div>
                     <div class="col">
                         <div class="form-group">
-                            <label for="OBR_OBSERVACION">Observacion<span style="color:#FF0000";></span></label>
-                        <input type="text" class="form-control" id="OBR_OBSERVACION" value="{{ $objeto->OBR_OBSERVACION }}" name="OBR_OBSERVACION"  >
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="OBR_FECHA_RECEPCION">Fecha Recepcion<span style="color:#FF0000";>*</span></label>
-                            <input type="date" class="form-control" id="OBR_FECHA_RECEPCION" name="OBR_FECHA_RECEPCION" value="{{ $objeto->OBR_FECHA_RECEPCION }}"  required>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="form-group">
-                            <label for="OBR_FECHA_ENTREGA">Fecha Entrega<span style="color:#FF0000";></span></label>
-                        <input type="date" class="form-control" id="OBR_FECHA_ENTREGA" name="OBR_FECHA_ENTREGA" value="{{ $objeto->OBR_FECHA_ENTREGA }}"  >
+                            <label for="NOT_FECHA_FIN">Fecha Fin<span style="color:#FF0000";></span></label>
+                        <input type="date" class="form-control" id="NOT_FECHA_FIN" name="NOT_FECHA_FIN"  value="{{ $noticia->NOT_FECHA_FIN
+                         }}"  required >
                         </div>
                     </div>
                 </div>
@@ -59,31 +52,12 @@
                         <div class="form-group">
                             <label for="image">Imagen<span style="color:#FF0000";>*</span></label>
                             </br>
-                            <img src="{{$objeto -> OBR_IMAGEN}}" width="100" height="100"/>
+                            <img src="{{$noticia -> NOT_IMAGEN}}" width="100" height="100"/>
                             </br>
-                            <input type="file"  class="form-control" id="image" name="image" accept="image/*" value="{{ $objeto->OBR_IMAGEN }}" required>
+                            <input type="file"  class="form-control" id="image" name="image" accept="image/*" value="{{ $noticia->NOT_IMAGEN }}" required>
                         </div>
                     </div>
-                    <div class="col">
-                      
-                        <div class="form-group">
-                            <label for="OBR_ESTADO">Estado Objeto<span style="color:#FF0000";>*</span></label>
-                            <select type="input" class="form-control" id="OBR_ESTADO" name="OBR_ESTADO" placeholder="Campus"  required>
-                                    @if($objeto->OBR_ESTADO == 0)
-                                        <option value="0" selected>DAÑADO</option>
-                                        <option value="1" >FUNCIONAL</option>
-                                    @else
-                                        @if($objeto->OBR_ESTADO == 1)
-                                        
-                                            <option value="1" selected>FUNCIONAL</option>
-                                            <option value="0" >DAÑADO</option>
-                                        @endif
-                                    @endif
-                                 
-                                
-                            </select> 
-                        </div>
-                    </div>
+
                 </div>
                 <div class="row">
                     <div class="col">
@@ -91,7 +65,7 @@
                             <label for="PER_CODIGO">Periodo<span style="color:#FF0000";>*</span></label>
                             <select type="input" class="form-control" id="PER_CODIGO" name="PER_CODIGO" placeholder="Campus"  required>
                             @foreach ($periodo as $per)
-                                    @if($per->PER_CODIGO == $objeto->PER_CODIGO)
+                                    @if($per->PER_CODIGO == $noticia->PER_CODIGO)
                                         <option value="{{$per->PER_CODIGO}}" selected>{{$per->PER_NOMBRE}}</option>
                                     @else
                                         <option  value="{{$per->PER_CODIGO}}">{{$per->PER_NOMBRE}}</option>
@@ -107,7 +81,7 @@
                         <label for="EMP_CODIGO">Empresa<span style="color:#FF0000";>*</span></label>
                         <select type="input" class="form-control" id="EMP_CODIGO" name="EMP_CODIGO" placeholder="Empresa"  required>
                             @foreach ($empresas as $emp)
-                                    @if($emp->EMP_CODIGO == $objeto->EMP_CODIGO)
+                                    @if($emp->EMP_CODIGO == $noticia->EMP_CODIGO)
                                         <option value="{{$emp->EMP_CODIGO}}" selected>{{$emp->EMP_NOMBRE}}</option>
                                     @else
                                         <option  value="{{$emp->EMP_CODIGO}}">{{$emp->EMP_NOMBRE}}</option>
